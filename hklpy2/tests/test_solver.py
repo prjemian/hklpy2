55c2630c6dd8a66472f525efb4fe8629d6325109
Pete R Jemian <prjemian@gmail.com>
2024-05-06 18:02:23 -0500

    TST

--- hklpy2/tests/test_solver.py
+++ hklpy2/tests/test_solver.py
@@ -19,3 +19,24 @@ def test_solvers(solver_name):
     solver = solver_class()
     assert isinstance(solver.__version__, str)
     assert len(solver.__version__) > 0, f"{solver.__version__=}"
+
+
+def test_HklSolver():
+    Solver = entry_points(group="hklpy2.solver")["hkl_soleil"].load()
+    assert Solver is not None
+
+    solver = Solver()
+    assert solver is not None
+    assert isinstance(solver.__version__, str)
+
+    gname = "ESRF ID01 PSIC"
+    solver.chooseGeometry(gname)
+    assert solver._geometry is not None
+    assert solver.gname == gname
+    assert solver._geometry.name_get() == gname
+
+    reals = solver.real_axis_names()
+    assert reals == "mu eta phi nu delta".split()
+
+    pseudos = solver.pseudo_axis_names()
+    assert pseudos == "h k l".split()
