9855366fa51331df22075f5d6ef52e76f63920c4
prjemian <prjemian@users.noreply.github.com>
2024-05-08 19:02:49 -0500

    deploy: 2710cdb557cef7e1451822116fcc747ad0399b40

---
+++ _sources/guide.rst.txt
@@ -0,0 +1,15 @@
+.. include:: /substitutions.txt
+
+.. _user_guide:
+
+==========
+User Guide
+==========
+
+TODO
+
+.. toctree::
+   :glob:
+   :hidden:
+
+   notebooks/*
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev1+gb4898da',
+    VERSION: '0.1.dev1+g2710cdb',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
