7bbfe419249f0b43066f2787c080fdb8fd17f9d7
Pete R Jemian <prjemian@gmail.com>
2024-05-10 00:56:01 -0500

    TST forward & inverse

--- hklpy2/backends/tests/test_th_tth_q.py
+++ hklpy2/backends/tests/test_th_tth_q.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pytest
+
 from ... import get_solver
 from .. import SolverBase
 from ..th_tth_q import ThTthSolver
@@ -25,19 +28,41 @@ def test_solver():
     solver.mode = "bisector"
     assert solver.mode == "bisector"
 
-    solver.wavelength = 1.0
-    result = solver.inverse({"tth": 10.0})
-    assert isinstance(result, dict)
-    assert "q" in result
-    assert round(result["q"], 4) == 1.0952, f"{result=!r}"
 
-    result = solver.forward(result)
-    assert isinstance(result, list)
-    assert len(result) == 1
-
-    result = result[0]
+@pytest.mark.parametrize(
+    "transform, wavelength, inputs, outputs, tol",
+    [
+        ["inverse", 0.5, {"th": 60, "tth": 120}, {"q": 21.7655924}, 0.0001],
+        ["forward", 0.5, {"q": 21.7655924}, {"th": 60, "tth": 120}, 0.0001],
+        ["inverse", 1.0, {"th": 5, "tth": 10}, {"q": 1.095231}, 0.0001],
+        ["forward", 1.0, {"q": 1.095231}, {"th": 5, "tth": 10}, 0.0001],
+        ["inverse", 1.54, {"th": 14.9131, "tth": 29.8262}, {"q": 2.1}, 0.0001],
+        ["forward", 1.54, {"q": 2.1}, {"th": 14.9131, "tth": 29.8262}, 0.0001],
+        ["inverse", 2.1, {"th": 1, "tth": 2}, {"q": 0.104435}, 0.0001],
+        ["forward", 2.1, {"q": 0.104435}, {"th": 1, "tth": 2}, 0.0001],
+        ["inverse", 2.1, {"th": 0.1, "tth": 0.2}, {"q": 0.0104440}, 0.0001],
+        ["forward", 2.1, {"q": 0.0104440}, {"th": 0.1, "tth": 0.2}, 0.0001],
+        ["inverse", 2.1, {"th": 0.01, "tth": 0.02}, {"q": 0.00104440}, 0.0001],
+        ["forward", 2.1, {"q": 0.00104440}, {"th": 0.01, "tth": 0.02}, 0.0001],
+    ],
+)
+def test_transforms(transform, wavelength, inputs, outputs, tol):
+    solver = get_solver("th_tth")()
+    solver.geometry = "TH TTH Q"
+    solver.mode = "bisector"
+    solver.wavelength = wavelength
+    if transform == "forward":
+        result = solver.forward(inputs)
+        assert isinstance(result, list)
+        assert len(result) == 1
+        result = result[0]
+        assert "th" in result
+        assert "tth" in result
+    elif transform == "inverse":
+        result = solver.inverse(inputs)
+        assert "q" in result
     assert isinstance(result, dict)
-    assert "th" in result
-    assert "tth" in result
-    assert round(result["th"], 4) == 5, f"{result=!r}"
-    assert round(result["tth"], 4) == 10, f"{result=!r}"
+    assert list(result.keys()) == list(outputs.keys())
+    arr_r = np.array(list(result.values()))
+    arr_o = np.array(list(outputs.values()))
+    assert np.allclose(arr_r, arr_o, atol=tol), f"{result=}  {outputs=}"
