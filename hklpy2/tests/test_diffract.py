b8b40a33443b9e6a603ee077d8e8c7e6b403c91e
Pete R Jemian <prjemian@gmail.com>
2024-05-21 18:25:09 -0500

    TST  re-enable

--- hklpy2/tests/test_diffract.py
+++ hklpy2/tests/test_diffract.py
@@ -1,6 +1,7 @@
 """Test the hklpy2.diffract module."""
 
 import math
+from contextlib import nullcontext as does_not_raise
 
 import pytest
 from ophyd import Component as Cpt
@@ -8,17 +9,12 @@ from ophyd import Kind
 from ophyd import PseudoSingle
 from ophyd import SoftPositioner
 
+from .. import SolverBase
 from ..diffract import DiffractometerBase
 from ..diffract import DiffractometerError
-
-# from ..misc import get_solver
-# from ..misc import solver_factory
+from ..misc import solver_factory
 from ..wavelength_support import DEFAULT_WAVELENGTH
 from ..wavelength_support import DEFAULT_WAVELENGTH_UNITS
-
-# from contextlib import nullcontext as does_not_raise
-
-
 
 NORMAL_HINTED = Kind.hinted | Kind.normal
 
@@ -30,16 +26,24 @@ class Fourc(DiffractometerBase):
     k = Cpt(PseudoSingle, "", kind=NORMAL_HINTED)  # noqa: E741
     l = Cpt(PseudoSingle, "", kind=NORMAL_HINTED)  # noqa: E741
 
-    omega = Cpt(SoftPositioner, limits=(-180, 180), init_pos=0, kind=NORMAL_HINTED)
+    theta = Cpt(SoftPositioner, limits=(-180, 180), init_pos=0, kind=NORMAL_HINTED)
     chi = Cpt(SoftPositioner, limits=(-180, 180), init_pos=0, kind=NORMAL_HINTED)
     phi = Cpt(SoftPositioner, limits=(-180, 180), init_pos=0, kind=NORMAL_HINTED)
-    tth = Cpt(SoftPositioner, limits=(-170, 170), init_pos=0, kind=NORMAL_HINTED)
+    ttheta = Cpt(SoftPositioner, limits=(-170, 170), init_pos=0, kind=NORMAL_HINTED)
+
+    # define a few more axes, extra parameters for some geometries/engines/modes
 
     h2 = Cpt(PseudoSingle, "", kind=NORMAL_HINTED)  # noqa: E741
     k2 = Cpt(PseudoSingle, "", kind=NORMAL_HINTED)  # noqa: E741
     l2 = Cpt(PseudoSingle, "", kind=NORMAL_HINTED)  # noqa: E741
-
     psi = Cpt(SoftPositioner, limits=(-170, 170), init_pos=0, kind=NORMAL_HINTED)
+
+    # and a few more axes not used by 4-circle code
+
+    q = Cpt(PseudoSingle, "", kind=NORMAL_HINTED)  # noqa: E741
+    mu = Cpt(SoftPositioner, limits=(-170, 170), init_pos=0, kind=NORMAL_HINTED)
+    nu = Cpt(SoftPositioner, limits=(-170, 170), init_pos=0, kind=NORMAL_HINTED)
+    omicron = Cpt(SoftPositioner, limits=(-170, 170), init_pos=0, kind=NORMAL_HINTED)
 
 
 class Th2Th(DiffractometerBase):
@@ -72,62 +76,76 @@ def test_DiffractometerBase():
 
 
 @pytest.mark.parametrize(
-    "geometry, npseudos, nreals", [[Fourc, 6, 5], [Th2Th, 1, 2], [TwoC, 2, 3]]
+    "dclass, npseudos, nreals", [[Fourc, 7, 8], [Th2Th, 1, 2], [TwoC, 2, 3]]
 )
 @pytest.mark.parametrize(
     "solver, gname",
-    [["no_op", ""], ["hkl_soleil", "E4CV"], ["th_tth", "TH TTH Q"]],
+    [["no_op", "th_tth"], ["hkl_soleil", "E4CV"], ["th_tth", "TH TTH Q"]],
 )
-def test_goniometer(solver, gname, geometry, npseudos, nreals):
-    goniometer = geometry("", name="goniometer")
-    assert goniometer is not None
-    assert len(goniometer.pseudo_positioners) == npseudos
-    assert len(goniometer._pseudo) == npseudos
-    assert len(goniometer.real_positioners) == nreals
-    assert len(goniometer._real) == nreals
-    assert not goniometer.moving
+def test_goniometer(solver, gname, dclass, npseudos, nreals):
+    diffractometer = dclass("", name="goniometer")
+    assert diffractometer is not None
+    assert len(diffractometer.pseudo_positioners) == npseudos
+    assert len(diffractometer._pseudo) == npseudos
+    assert len(diffractometer.real_positioners) == nreals
+    assert len(diffractometer._real) == nreals
+    assert not diffractometer.moving
 
     # test the wavelength
-    assert math.isclose(goniometer.wavelength.get(), DEFAULT_WAVELENGTH, abs_tol=0.001)
-    assert goniometer.wavelength_units.get() == DEFAULT_WAVELENGTH_UNITS
+    assert math.isclose(
+        diffractometer.wavelength.get(),
+        DEFAULT_WAVELENGTH,
+        abs_tol=0.001,
+    )
+    assert diffractometer.wavelength_units.get() == DEFAULT_WAVELENGTH_UNITS
 
-    # # test the solver
-    # assert hasattr(goniometer, "solver_name")
-    # assert goniometer.solver_name == "", f"{goniometer.solver_name=!r}"
-    # assert hasattr(goniometer, "solver")
-    # with does_not_raise() as reason:
-    #     value = goniometer.solver.get()
-    # assert reason is None
-    # assert isinstance(value, str), f"{value=!r} {reason=!r}"
-    # assert value == "", f"{value=!r} {solver=!r} {reason=!r}"
+    # test the solver
+    diffractometer.set_solver(solver, geometry=gname)
+    assert hasattr(diffractometer, "solver_name")
+    assert hasattr(diffractometer, "_solver")
+    assert diffractometer._solver is not None
+    assert isinstance(diffractometer._solver, SolverBase)
+    assert isinstance(diffractometer._solver.name, str)
+    assert diffractometer.solver_name == solver, f"{diffractometer.solver_name=!r}"
+    assert hasattr(diffractometer, "solver")
+    with does_not_raise() as reason:
+        value = diffractometer.solver.get()
+    assert reason is None
+    assert isinstance(value, str), f"{value=!r} {reason=!r}"
+    assert value == solver, f"{value=!r} {solver=!r} {reason=!r}"
 
-    # with does_not_raise() as reason:
-    #     goniometer.position
-    # assert reason is None
-    # with does_not_raise() as reason:
-    #     goniometer.report
-    # assert reason is None
+    with does_not_raise() as reason:
+        diffractometer.position
+    assert reason is None
+    with does_not_raise() as reason:
+        diffractometer.report
+    assert reason is None
 
-    # solver_object = solver_factory(solver, geometry=gname)
-    # assert solver_object is not None
-    # assert solver_object.name == solver
+    solver_object = solver_factory(solver, geometry=gname)
+    assert solver_object is not None
+    assert solver_object.name == solver
 
 
 def test_extras():
-    # solver_name = "hkl_soleil"
+    solver_name = "hkl_soleil"
+    gname = "E4CV"
     fourc = Fourc("", name="fourc")
     assert fourc is not None
-    # , solver=solver_name, geometry="E4CV"
-    # assert "solver_name" in dir(fourc), f"{dir(fourc)!r}"
-    # assert fourc.solver_name == solver_name, f"{fourc!r}"
-    # assert fourc.solver.get() == solver_name
 
-    # solver_klass = get_solver(solver_name)
-    # solver = solver_klass(
-    #     geometry="E4CV",
-    #     pseudos=[fourc.h, fourc.k, fourc.l],  # TODO: add to constructor
-    #     reals=[fourc.omega, fourc.chi, fourc.phi, fourc.tth],
-    #     extras=[fourc.h2, fourc.k2, fourc.l2, fourc.psi],
-    # )
-    # assert solver is not None
+    fourc.set_solver(
+        solver_name,
+        geometry=gname,
+        pseudos=[fourc.h, fourc.k, fourc.l],
+        reals=[fourc.theta, fourc.chi, fourc.phi, fourc.ttheta],
+        extras=[fourc.h2, fourc.k2, fourc.l2, fourc.psi],
+    )
+    assert "solver_name" in dir(fourc), f"{dir(fourc)!r}"
+    assert fourc.solver_name == solver_name, f"{fourc!r}"
+    assert fourc.solver.get() == solver_name
+
+    fourc._solver.mode = "psi_constant"
+    assert fourc._solver.pseudo_axis_names == "h k l".split()
+    assert fourc._solver.real_axis_names == "omega chi phi tth".split()
+    assert fourc._solver.extra_axis_names == "h2 k2 l2 psi".split()
+
     # TODO:
