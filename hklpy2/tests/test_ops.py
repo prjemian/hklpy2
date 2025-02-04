265a5811f6fe0166d8d3a8bfa4833ecbe3231d9d
Pete R Jemian <prjemian@gmail.com>
2025-02-04 12:21:26 -0600

    TST #15

--- hklpy2/tests/test_ops.py
+++ hklpy2/tests/test_ops.py
@@ -7,20 +7,20 @@ import pytest
 from ..geom import SimulatedTheta2Theta
 from . import models
 
-SKIP_VALUE_TEST = str(uuid.uuid4())
+SKIP_EXACT_VALUE_TEST = str(uuid.uuid4())
 
 
 @pytest.mark.parametrize(
     "dclass, dname, keypath, value",
     [
-        [models.Fourc, "fourc", "_header", SKIP_VALUE_TEST],
+        [models.Fourc, "fourc", "_header", SKIP_EXACT_VALUE_TEST],
         [models.Fourc, "fourc", "name", "fourc"],
         [models.Fourc, "fourc", "axes.axes_xref", {}],
         [models.Fourc, "fourc", "geometry", "E4CV"],
         [models.Fourc, "fourc", "solver.name", "hkl_soleil"],
-        [models.Fourc, "fourc", "samples", SKIP_VALUE_TEST],
-        [models.Fourc, "fourc", "solver.version", SKIP_VALUE_TEST],
-        [SimulatedTheta2Theta, "t2t", "_header", SKIP_VALUE_TEST],
+        [models.Fourc, "fourc", "samples", SKIP_EXACT_VALUE_TEST],
+        [models.Fourc, "fourc", "solver.version", SKIP_EXACT_VALUE_TEST],
+        [SimulatedTheta2Theta, "t2t", "_header", SKIP_EXACT_VALUE_TEST],
         [SimulatedTheta2Theta, "t2t", "name", "t2t"],
         [
             SimulatedTheta2Theta,
@@ -35,14 +35,18 @@ SKIP_VALUE_TEST = str(uuid.uuid4())
     ],
 )
 def test_asdict(dclass, dname, keypath, value):
+    """."""
     fourc = dclass(name=dname)
 
     db = fourc.operator._asdict()
     assert db["name"] == dname
 
+    # Walk through the keypath, revising the db object at each step
     for k in keypath.split("."):
         db = db.get(k)  # narrow the search
-        assert db is not None, f"{k=!r}"
+        assert db is not None, f"{k=!r}"  # Ensure the path exists, so far.
 
-    if value != SKIP_VALUE_TEST:
+    if value == SKIP_EXACT_VALUE_TEST:
+        assert value is not None  # Anything BUT 'None'
+    else:
         assert value == db, f"{value=!r}  {db=!r}"
